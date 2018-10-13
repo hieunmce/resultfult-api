a10caca3db5f9febc516b2e2262c6b2f513bcc89
hieunmce <hieunmce@gmail.com>
2018-10-13 22:09:05 +0700

    add create user and not found path checking

--- src/resultfult_api/core.clj
+++ src/resultfult_api/core.clj
@@ -1,6 +1,10 @@
 (ns resultfult-api.core
   (:require [toucan.db :as db]
-            [toucan.models :as models])
+            [toucan.models :as models]
+            [ring.adapter.jetty :refer [run-jetty]]
+            [compojure.route]
+            [compojure.api.sweet :refer [api routes undocumented]]
+            [resultfult-api.user :refer [user-routes]])
   (:gen-class))
 
 (def db-spec
@@ -9,8 +13,11 @@
    :user "postgres"
    :password "example"})
 
+(def app (api (apply routes user-routes (undocumented (compojure.route/not-found (ring.util.http-response/not-found {:not "found"}))))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (db/set-default-db-connection! db-spec)
-  (models/set-root-namespace! 'resultfult-api.models))
+  (models/set-root-namespace! 'resultfult-api.models)
+  (run-jetty app {:port 3000}))
--- src/resultfult_api/user.clj
+++ src/resultfult_api/user.clj
@@ -1,6 +1,12 @@
 (ns resultfult-api.user
   (:require [schema.core :as s]
-            [resultfult-api.string-util :as str]))
+            [buddy.hashers :as hashers]
+            [resultfult-api.string-util :as str]
+            [clojure.set :refer [rename-keys]]
+            [toucan.db :as db]
+            [resultfult-api.models.user :refer [User]]
+            [ring.util.http-response :refer [created]]
+            [compojure.api.sweet :refer [POST]]))
 
 (defn valid-username? [name]
   (str/non-blank-with-max-length? 50 name))
@@ -13,3 +19,20 @@
    :password (s/constrained s/Str valid-password?)
    :email (s/constrained s/Str str/email?)})
 
+(defn id->created [id]
+  (created (str "/users/" id) {:id id}))
+
+(defn canonicalize-user-req [user-req]
+  (-> (update user-req :password hashers/derive)
+      (rename-keys {:password :password_hash})))
+
+(defn create-user-handler [create-user-req]
+  (->> (canonicalize-user-req create-user-req)
+       (db/insert! User)
+       :id
+       id->created))
+
+(def user-routes
+  [(POST "/users" []
+     :body [create-user-req UserRequestSchema]
+     (create-user-handler create-user-req))])
