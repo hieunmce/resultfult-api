5add83ae35e0be62bc76d0f5df6b9903cecd8ba2
hieunmce <hieunmce@gmail.com>
2018-10-15 23:19:53 +0700

    add swagger API

--- src/resultfult_api/app.clj
+++ src/resultfult_api/app.clj
@@ -1,15 +1,20 @@
 (ns resultfult-api.app
-  (:require [ring.middleware.reload :refer [wrap-reload]]
-            [compojure.route :refer [not-found]]
+  (:require [compojure.route :refer [not-found]]
             [compojure.api.sweet :refer [api routes undocumented]]
             [resultfult-api.user :refer [user-routes]])
   (:gen-class))
+
+(def swagger-config
+  {:ui "/swagger"
+   :spec "/swagger.json"
+   :options {:ui {:validatorUrl nil}
+             :data {:info {:version "1.0.0", :title "Restful CRUD API"}}}})
 
 (def not-found-routes
   (not-found
    (ring.util.http-response/not-found {:not "found"})))
 
 (def create-app
-  (api
-   (wrap-reload (apply routes user-routes
-                       (undocumented not-found-routes)))))
+  (api {:swagger swagger-config}
+       (apply routes user-routes
+              (undocumented not-found-routes))))
